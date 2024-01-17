eecc17202dcbfd463201ee04bcdd845816ad016d
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-01-17 15:53:04 +0100

    Option to select language added

--- GradesApp/GradesApp/Program.cs
+++ GradesApp/GradesApp/Program.cs
@@ -1,38 +1,82 @@
 ﻿using GradesApp;
 
-//var student1 = new Student("Ania", "Gil");
-//var student2 = new Student("Ola", "Sowa");
-//var student3 = new Student("Pati", "Kot");
-
-//student1.AddGrade(5);
-//student1.AddGrade(4);
-//student1.AddGrade("33");
-//student1.AddGrade("Aaaa");
-//student1.AddGrade(3.5);
-//student1.AddGrade(5);
-//student1.AddGrade(5);
-
-Console.WriteLine("Witamy w dzienniku elektronicznym!");
-Console.WriteLine("==================================");
-Console.WriteLine();
-Console.WriteLine("Dodaj ocenę:");
-
-var student = new Student("Anna", "Kos");
+string selectEnglish = "Please select language: press 1 for English or 2 for Polish. If you want to quit, press 'q'.";
+string selectPolish = "Proszę wybrać język: naciśnij 1 dla angielskiego lub 2 dla polskiego. Jeśli chcesz zamknąć dziennik naciśnij 'q'.";
+Console.WriteLine(selectEnglish);
+Console.WriteLine(selectPolish);
 
 while (true)
 {
-    var input = Console.ReadLine();
-    if (input == "q")
+    var languageInput = Console.ReadLine();
+    if (languageInput == "1")
     {
-        break;
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the electronic journal!");
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine("Add a grade:");
+
+        var student = new Student("Anna", "Kos");
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "q")
+            {
+                break;
+            }
+            student.AddGrade(input);
+            Console.WriteLine("Add another grade:");
+        }
+
+        var statistics = student.GetStatistics();
+
+        Console.WriteLine($"Average: {statistics.Average:N2}");
+        Console.WriteLine($"Min: {statistics.Min}");
+        Console.WriteLine($"Max: {statistics.Max}");
+        Console.WriteLine(statistics.AverageWord);
     }
-    student.AddGrade(input);
-    Console.WriteLine("Dodaj kolejną ocenę:");
+    else if (languageInput == "2")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Witamy w dzienniku elektronicznym!");
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine("Dodaj ocenę:");
+
+        var student = new Student("Anna", "Kos");
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "q")
+            {
+                break;
+            }
+            student.AddGrade(input);
+            Console.WriteLine("Dodaj kolejną ocenę:");
+        }
+
+        var statistics = student.GetStatistics();
+
+        Console.WriteLine($"Average: {statistics.Average:N2}");
+        Console.WriteLine($"Min: {statistics.Min}");
+        Console.WriteLine($"Max: {statistics.Max}");
+        Console.WriteLine(statistics.AverageWord);
+    }
+    else if (languageInput == "q")
+    {
+        Console.WriteLine();
+        Console.WriteLine("See you soon!");
+        Console.WriteLine("Do zobaczenia!");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Invalid character. You can only enter 1 or 2 or 'q'. Try again.");
+        Console.WriteLine("Wprowadzono nieprawidłowy znak. Możesz wprowadzić jedynie 1 lub 2 lub 'q'. Spróbuj ponownie.");
+        Console.WriteLine("------------------------------------------------------------------------------------");
+        Console.WriteLine(selectEnglish);
+        Console.WriteLine(selectPolish);
+    }
 }
-
-var statistics = student.GetStatistics();
-
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine(statistics.AverageWord);
